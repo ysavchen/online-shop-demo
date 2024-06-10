50e6ea2f6b56125271a64ec963dccff00da49137
Yuriy Savchenko <ysavchen@outlook.com>
2024-06-10 22:37:55 +0300

    Delete frontend directory

--- frontend/src/mixins/form-rules.js
+++
@@ -1,9 +0,0 @@
-export default {
-    data: () => ({
-        rules: {
-            required: value => !!value || 'Required.',
-            minChars: v => (v && v.length >= 8) || 'Min 8 characters',
-            emailValidity: v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-        }
-    }),
-}
