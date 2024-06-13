fbc45188ba6687f5f696aa1171e09d2d95e05880
Yuriy Savchenko <ysavchen@outlook.com>
2024-06-13 16:18:40 +0300

    Update controllers

--- backend/src/main/java/com/mycompany/online_shop_backend/controller/OrderController.java
+++ backend/src/main/java/com/mycompany/online_shop_backend/controller/OrderController.java
@@ -3,9 +3,9 @@ package com.mycompany.online_shop_backend.controller;
 import com.mycompany.online_shop_backend.controller.model.request.OrderRequest;
 import com.mycompany.online_shop_backend.controller.model.response.CreatedOrderResponse;
 import com.mycompany.online_shop_backend.controller.model.response.UserOrderDto;
-import com.mycompany.online_shop_backend.service.model.OrderDto;
 import com.mycompany.online_shop_backend.service.OrderService;
 import com.mycompany.online_shop_backend.service.SecurityService;
+import com.mycompany.online_shop_backend.service.model.OrderDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -16,26 +16,20 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class OrderController {
 
     private final OrderService orderService;
     private final SecurityService securityService;
 
-    @PostMapping(
-            path = "/api/v1/orders",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
+    @PostMapping(path = "/api/v1/orders", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public CreatedOrderResponse createOrder(@RequestBody OrderRequest request) {
         OrderDto orderDto = orderService.save(request);
         return CreatedOrderResponse.toDto(orderDto);
     }
 
-    @GetMapping(
-            path = "/api/v1/users/{id}/orders",
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
+    @GetMapping(path = "/api/v1/users/{id}/orders")
     public List<UserOrderDto> getUserOrders(HttpServletRequest request) {
         String email = securityService.getUsernameFromRequest(request);
         List<OrderDto> orderDtos = orderService.getOrdersByEmail(email);
