65c5e711d55dad8b1e98b239fb98b30995c6477c
Yuriy Savchenko <ysavchen@outlook.com>
2024-07-01 02:37:42 +0300

    Update db scripts

--- book-service/src/main/resources/liquibase/v-1.0/002.sql
+++ book-service/src/main/resources/liquibase/v-1.0/002.sql
@@ -1,28 +1,31 @@
 --liquibase formatted sql
 
---changeset Savchenko Y.A.:002.01 runOnChange:false splitStatements:true runInTransaction:false
+--changeset ysavchen:002.01 runOnChange:false splitStatements:true runInTransaction:false
 CREATE TABLE IF NOT EXISTS books
 (
     id          uuid             PRIMARY KEY DEFAULT MD5(RANDOM()::text || CLOCK_TIMESTAMP()::text)::uuid,
-    title       varchar(255)     NOT NULL,
+    title       varchar(150)     NOT NULL,
     authors     text[]           NOT NULL DEFAULT '{}',
-    description text,
-    price       double precision NOT NULL,
+    description text,                      --todo: добавить валидацию на base64
+    quantity    smallint         NOT NULL,
+    price       numeric(12, 2)   NOT NULL,
     currency    varchar(3)       NOT NULL,
     created_at  timestamptz      NOT NULL DEFAULT NOW(),
     updated_at  timestamptz      NOT NULL DEFAULT NOW()
 );
 
 COMMENT ON TABLE books IS 'Таблица для хранения книг';
+COMMENT ON COLUMN books.id IS 'Идентификатор записи, первичный ключ';
 COMMENT ON COLUMN books.title IS 'Название книги';
 COMMENT ON COLUMN books.authors IS 'Авторы книги';
 COMMENT ON COLUMN books.description IS 'Описание книги в base64';
+COMMENT ON COLUMN books.quantity IS 'Количество книг на складе';
 COMMENT ON COLUMN books.price IS 'Цена книги';
 COMMENT ON COLUMN books.currency IS 'Валюта для цены книги';
 COMMENT ON COLUMN books.created_at IS 'Дата и время создания записи';
 COMMENT ON COLUMN books.updated_at IS 'Дата и время последнего изменения записи';
 
---changeset Savchenko Y.A.:002.02 runOnChange:false splitStatements:false runInTransaction:false
+--changeset ysavchen:002.02 runOnChange:false splitStatements:false runInTransaction:false
 DO
 $do$
     BEGIN
