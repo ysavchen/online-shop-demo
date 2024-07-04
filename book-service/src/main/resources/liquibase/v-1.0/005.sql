25f67dbe3488a37e420c1d0eecd6c261ca38699f
Yuriy Savchenko <ysavchen@outlook.com>
2024-07-04 04:19:41 +0300

    Update sql scripts

--- book-service/src/main/resources/liquibase/v-1.0/005.sql
+++ book-service/src/main/resources/liquibase/v-1.0/005.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 
 --changeset ysavchen:005.01 runOnChange:false splitStatements:false runInTransaction:false
-CREATE OR REPLACE FUNCTION get_book_id_func(title varchar(150)) RETURNS uuid AS
+CREATE OR REPLACE FUNCTION get_book_id(title varchar(150)) RETURNS uuid AS
 $$
 DECLARE book_id uuid;
 BEGIN
