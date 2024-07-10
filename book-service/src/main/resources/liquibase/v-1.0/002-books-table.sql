af7ba96161f7d6f3960925ec1612380c7f999ea8
Yuriy Savchenko <ysavchen@outlook.com>
2024-07-10 05:11:16 +0300

    Add column release date

--- book-service/src/main/resources/liquibase/v-1.0/002-books-table.sql
+++ book-service/src/main/resources/liquibase/v-1.0/002-books-table.sql
@@ -3,17 +3,17 @@
 --changeset ysavchen:002.01 runOnChange:false splitStatements:true runInTransaction:false
 CREATE TABLE IF NOT EXISTS books
 (
-    id          uuid             PRIMARY KEY DEFAULT MD5(RANDOM()::text || CLOCK_TIMESTAMP()::text)::uuid,
-    title       varchar(150)     NOT NULL,
-    authors     text[]           NOT NULL DEFAULT '{}',
-    description text,
-    genre       varchar(50)      NOT NULL,
-    --todo release_date
-    quantity    smallint         NOT NULL,
-    price       numeric(12, 2),
-    currency    varchar(3),
-    created_at  timestamptz      NOT NULL DEFAULT NOW(),
-    updated_at  timestamptz      NOT NULL DEFAULT NOW()
+    id           uuid             PRIMARY KEY DEFAULT MD5(RANDOM()::text || CLOCK_TIMESTAMP()::text)::uuid,
+    title        varchar(150)     NOT NULL,
+    authors      text[]           NOT NULL DEFAULT '{}',
+    description  text,
+    genre        varchar(50)      NOT NULL,
+    release_date date,
+    quantity     smallint         NOT NULL,
+    price        numeric(12, 2),
+    currency     varchar(3),
+    created_at   timestamptz      NOT NULL DEFAULT NOW(),
+    updated_at   timestamptz      NOT NULL DEFAULT NOW()
 );
 
 COMMENT ON TABLE books IS 'Таблица для хранения книг';
@@ -22,7 +22,7 @@ COMMENT ON COLUMN books.title IS 'Название книги';
 COMMENT ON COLUMN books.authors IS 'Авторы книги';
 COMMENT ON COLUMN books.description IS 'Описание книги в base64';
 COMMENT ON COLUMN books.genre IS 'Жанр книги';
---todo COMMENT ON COLUMN books.release_date IS 'Дата выхода книги';
+COMMENT ON COLUMN books.release_date IS 'Дата выхода книги';
 COMMENT ON COLUMN books.quantity IS 'Количество книг на складе';
 COMMENT ON COLUMN books.price IS 'Цена книги';
 COMMENT ON COLUMN books.currency IS 'Валюта для цены книги';
