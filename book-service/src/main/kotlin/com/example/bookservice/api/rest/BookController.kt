36eb11dc449396ecf302054440e16b82efdd9910
Yuriy Savchenko <ysavchen@outlook.com>
2024-07-12 15:29:18 +0300

    Update pagination logic

--- book-service/src/main/kotlin/com/example/bookservice/api/rest/BookController.kt
+++ book-service/src/main/kotlin/com/example/bookservice/api/rest/BookController.kt
@@ -3,6 +3,7 @@ package com.example.bookservice.api.rest
 import com.example.bookservice.api.rest.RestCompanion.BASE_PATH_V1
 import com.example.bookservice.api.rest.model.*
 import com.example.bookservice.service.BookService
+import org.springframework.data.domain.Page
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 import java.util.*
@@ -16,7 +17,7 @@ class BookController(private val bookService: BookService) {
         paging: Paging,
         sorting: Sorting,
         @RequestBody request: BookSearchRequest?
-    ): List<Book> = bookService.getBooks(paging, sorting, request)
+    ): Page<Book> = bookService.getBooks(paging, sorting, request)
 
     @GetMapping("/books/{bookId}")
     fun bookById(@PathVariable("bookId") bookId: UUID): Book = bookService.getBookById(bookId)
--- book-service/src/main/kotlin/com/example/bookservice/service/BookService.kt
+++ book-service/src/main/kotlin/com/example/bookservice/service/BookService.kt
@@ -3,6 +3,8 @@ package com.example.bookservice.service
 import com.example.bookservice.api.rest.model.*
 import com.example.bookservice.mapping.BookMapper.toModel
 import com.example.bookservice.repository.BookRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -12,8 +14,10 @@ import java.util.*
 class BookService(private val bookRepository: BookRepository) {
 
     @Transactional(readOnly = true)
-    fun getBooks(paging: Paging, sorting: Sorting, request: BookSearchRequest?): List<Book> =
-        bookRepository.findAll().map { it.toModel() }
+    fun getBooks(paging: Paging, sorting: Sorting, request: BookSearchRequest?): Page<Book> {
+        val pageRequest = PageRequest.of(paging.page, paging.pageSize)
+        return bookRepository.findAll(pageRequest).map { it.toModel() }
+    }
 
     @Transactional(readOnly = true)
     fun getBookById(bookId: UUID): Book = bookRepository.findByIdOrNull(bookId)?.toModel()
