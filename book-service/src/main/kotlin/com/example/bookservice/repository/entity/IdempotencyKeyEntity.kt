567cd247474ecf71bcc75fc458cd5f8f7884f05d
Yuriy Savchenko <ysavchen@outlook.com>
2024-07-14 00:40:49 +0300

    Update IdempotencyKeyEntity

--- book-service/src/main/kotlin/com/example/bookservice/repository/entity/IdempotencyKeyEntity.kt
+++ book-service/src/main/kotlin/com/example/bookservice/repository/entity/IdempotencyKeyEntity.kt
@@ -4,9 +4,12 @@ import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import org.hibernate.annotations.Immutable
+
 import java.util.*
 
 @Entity
+@Immutable
 @Table(name = "idempotency_keys")
 data class IdempotencyKeyEntity(
     @Id
