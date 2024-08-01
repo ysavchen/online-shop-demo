453b03cf1e972248a3768c80c848e975a0dad21a
Yuriy Savchenko <ysavchen@outlook.com>
2024-08-01 23:40:34 +0300

    Update reviews script

--- book-service/src/main/resources/liquibase/v-1.0.0/003-reviews-table.sql
+++ book-service/src/main/resources/liquibase/v-1.0.0/003-reviews-table.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS reviews
     review_text text,
     author      varchar(100)  NOT NULL,
     rating      numeric(2, 1) NOT NULL CONSTRAINT valid_rating CHECK(rating BETWEEN 1.0 AND 5.0),
-    book_fk     uuid          NOT NULL,
+    book_fk     uuid          NOT NULL REFERENCES books(id),
     created_at  timestamptz   NOT NULL DEFAULT NOW(),
     updated_at  timestamptz   NOT NULL DEFAULT NOW()
 );
@@ -23,14 +23,14 @@ COMMENT ON COLUMN reviews.book_fk IS 'Внешний ключ на запись 
 COMMENT ON COLUMN reviews.created_at IS 'Дата и время создания записи';
 COMMENT ON COLUMN reviews.updated_at IS 'Дата и время последнего изменения записи';
 
---changeset ysavchen:003.02 runOnChange:false splitStatements:true runInTransaction:false
-ALTER TABLE reviews
-    ADD CONSTRAINT fk_reviews_to_books FOREIGN KEY (book_fk) REFERENCES books (id);
+--alternative way to add constraint
+--ALTER TABLE reviews
+--    ADD CONSTRAINT fk_reviews_to_books FOREIGN KEY (book_fk) REFERENCES books(id);
 
---changeset ysavchen:003.03 runOnChange:false splitStatements:true runInTransaction:false
+--changeset ysavchen:003.02 runOnChange:false splitStatements:true runInTransaction:false
 CREATE INDEX IF NOT EXISTS book_fk_idx ON reviews (book_fk);
 
---changeset ysavchen:003.04 runOnChange:false splitStatements:false runInTransaction:false
+--changeset ysavchen:003.03 runOnChange:false splitStatements:false runInTransaction:false
 DO
 $do$
     BEGIN
