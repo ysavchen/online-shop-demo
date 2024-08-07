41774444b7f70eb9c8857b61b3afa7206cd56217
Yuriy Savchenko <ysavchen@outlook.com>
2024-08-07 12:34:54 +0300

    Fix review search by bookId

--- book-service/src/main/kotlin/com/example/bookservice/repository/ReviewRepository.kt
+++ book-service/src/main/kotlin/com/example/bookservice/repository/ReviewRepository.kt
@@ -1,7 +1,22 @@
 package com.example.bookservice.repository
 
+import com.example.bookservice.api.rest.model.ReviewSearchRequest
 import com.example.bookservice.repository.entity.ReviewEntity
+import com.example.bookservice.repository.entity.ReviewEntity_
+import org.springframework.data.jpa.domain.Specification
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import java.util.*
 
-interface ReviewRepository : JpaRepository<ReviewEntity, UUID>
+interface ReviewRepository : JpaRepository<ReviewEntity, UUID>, JpaSpecificationExecutor<ReviewEntity> {
+
+    companion object {
+
+        fun searchSpec(request: ReviewSearchRequest): Specification<ReviewEntity> = bookIdEqual(request.bookId)
+
+        private fun bookIdEqual(bookId: UUID): Specification<ReviewEntity> =
+            Specification { root, _, cb ->
+                cb.equal(root.get(ReviewEntity_.bookFk).`as`(UUID::class.java), bookId)
+            }
+    }
+}
--- book-service/src/main/kotlin/com/example/bookservice/service/ReviewService.kt
+++ book-service/src/main/kotlin/com/example/bookservice/service/ReviewService.kt
@@ -1,13 +1,14 @@
 package com.example.bookservice.service
 
 import com.example.bookservice.api.rest.ReviewNotFoundException
-import com.example.bookservice.api.rest.model.Review
 import com.example.bookservice.api.rest.ReviewRequestParams
+import com.example.bookservice.api.rest.model.Review
 import com.example.bookservice.api.rest.model.ReviewSearchRequest
 import com.example.bookservice.mapping.RequestMapper.toPageable
 import com.example.bookservice.mapping.ReviewMapper.toModel
 import com.example.bookservice.mapping.ReviewMapper.toPagedModel
 import com.example.bookservice.repository.ReviewRepository
+import com.example.bookservice.repository.ReviewRepository.Companion.searchSpec
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.data.web.PagedModel
 import org.springframework.stereotype.Service
@@ -19,7 +20,7 @@ class ReviewService(private val reviewRepository: ReviewRepository) {
 
     @Transactional(readOnly = true)
     fun getReviews(reviewRequestParams: ReviewRequestParams, request: ReviewSearchRequest): PagedModel<Review> =
-        reviewRepository.findAll(reviewRequestParams.toPageable()).toPagedModel()
+        reviewRepository.findAll(searchSpec(request), reviewRequestParams.toPageable()).toPagedModel()
 
     @Transactional(readOnly = true)
     fun getReviewById(reviewId: UUID): Review = reviewRepository.findByIdOrNull(reviewId)?.toModel()
