212ab62a1bcadec32efa4ca4925356facb4bd2f7
Yuriy Savchenko <ysavchen@outlook.com>
2024-08-11 14:16:19 +0300

    Fix test

--- order-service/src/main/kotlin/com/example/orderservice/api/rest/model/Errors.kt
+++ order-service/src/main/kotlin/com/example/orderservice/api/rest/model/Errors.kt
@@ -1,10 +1,13 @@
 package com.example.orderservice.api.rest.model
 
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.annotation.JsonFormat.Shape
 import java.time.OffsetDateTime
 import java.util.*
 
 data class ErrorResponse(
     val errorId: UUID,
+    @JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     val timestamp: OffsetDateTime,
     val path: String,
     val code: ErrorCode,
--- order-service/src/main/kotlin/com/example/orderservice/api/rest/model/Order.kt
+++ order-service/src/main/kotlin/com/example/orderservice/api/rest/model/Order.kt
@@ -1,5 +1,7 @@
 package com.example.orderservice.api.rest.model
 
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.annotation.JsonFormat.Shape
 import java.math.BigDecimal
 import java.time.OffsetDateTime
 import java.util.*
@@ -20,7 +22,9 @@ data class Order(
     val items: Set<Item>,
     val totalQuantity: Int,
     val totalPrice: TotalPrice,
+    @JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     val createdAt: OffsetDateTime,
+    @JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     val updatedAt: OffsetDateTime
 )
 
