f9a745c26faf0fdcc81d434e7927322aa41f583d
Yuriy Savchenko <ysavchen@outlook.com>
2024-08-11 14:25:39 +0300

    Update tests

--- order-service/src/test/kotlin/com/example/orderservice/api/rest/OrderControllerTests.kt
+++ order-service/src/test/kotlin/com/example/orderservice/api/rest/OrderControllerTests.kt
@@ -1,5 +1,6 @@
 package com.example.orderservice.api.rest
 
+import com.example.orderservice.api.rest.model.ErrorCode
 import com.example.orderservice.api.rest.model.OrderSearchRequest
 import com.example.orderservice.mapping.OrderMapper.toModel
 import com.example.orderservice.repository.OrderRepository
@@ -7,11 +8,14 @@ import com.example.orderservice.test.IntegrationTest
 import com.example.orderservice.test.OrderTestData.orderEntity
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.assertj.core.api.Assertions.assertThat
+import org.hamcrest.core.StringContains.containsString
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
+import java.util.*
 
 @IntegrationTest
 class OrderControllerTests {
@@ -60,7 +64,13 @@ class OrderControllerTests {
 
     @Test
     fun `get order by non-existing id`() {
-
+        mockMvc.get("/api/v1/orders/${UUID.randomUUID()}") {
+            accept = MediaType.APPLICATION_JSON
+        }.andExpect {
+            status { isNotFound() }
+            content { contentType(MediaType.APPLICATION_JSON) }
+            content { string(containsString(ErrorCode.RESOURCE_NOT_FOUND.name)) }
+        }
     }
 
     @Test
