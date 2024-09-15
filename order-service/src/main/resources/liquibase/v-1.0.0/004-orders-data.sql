bc63cd25766789d9880edc99ec77dd313daef80a
Yuriy Savchenko <ysavchen@outlook.com>
2024-09-16 00:19:51 +0300

    Fix hibernate error

--- order-service/src/main/resources/liquibase/v-1.0.0/004-orders-data.sql
+++ order-service/src/main/resources/liquibase/v-1.0.0/004-orders-data.sql
@@ -7,7 +7,7 @@ VALUES
     '0190bd82-13e5-77fe-bb73-0764d5e3efdb',
     '0190bd8a-8d20-72b2-a4a6-46de1cb92e06',
     'CREATED',
-    '[{"id": "01907545-eaf3-7de3-9157-dce3ece084e6", "category": "BOOK", "quantity": 1, "price": 720.00, "currency": "RUB"}]',
+    '[{"id": "01907545-eaf3-7de3-9157-dce3ece084e6", "category": "BOOK", "quantity": 1, "price": {"value": 720.00, "currency": "RUB"}}]',
     1,
     720.00,
     'RUB'
