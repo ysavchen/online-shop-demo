97159d229c7a6a64827bd73ce4a20d88196ef0f3
Yuriy Savchenko <ysavchen@outlook.com>
2024-09-20 00:18:02 +0300

    Configure ObjectMapper bean

--- 04-services/book-service/src/main/kotlin/com/example/bookservice/config/Configurations.kt
+++ 04-services/book-service/src/main/kotlin/com/example/bookservice/config/Configurations.kt
@@ -1,6 +1,14 @@
 package com.example.bookservice.config
 
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.MapperFeature
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.databind.json.JsonMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import jakarta.annotation.PostConstruct
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.ImportRuntimeHints
 import java.util.*
@@ -13,4 +21,24 @@ class ApplicationConfiguration {
     fun setTimezone() {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
     }
+}
+
+@Configuration
+class JsonConfiguration {
+
+    @Bean
+    fun objectMapper() = JsonMapper.builder()
+        .addModules(KotlinModule.Builder().build(), JavaTimeModule())
+        .enable(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN)
+        .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
+        .disable(
+            SerializationFeature.WRITE_DATES_AS_TIMESTAMPS,
+            SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS
+        )
+        .disable(
+            DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
+            DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS,
+            DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE
+        )
+        .build()
 }
--- 04-services/order-service/src/main/kotlin/com/example/orderservice/config/Configurations.kt
+++ 04-services/order-service/src/main/kotlin/com/example/orderservice/config/Configurations.kt
@@ -1,7 +1,15 @@
 package com.example.orderservice.config
 
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.MapperFeature
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.databind.json.JsonMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import jakarta.annotation.PostConstruct
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.ImportRuntimeHints
 import java.util.*
@@ -18,4 +26,24 @@ class ApplicationConfiguration(private val applicationProperties: ApplicationPro
     fun setTimezone() {
         TimeZone.setDefault(TimeZone.getTimeZone(applicationProperties.timezone))
     }
+}
+
+@Configuration
+class JsonConfiguration {
+
+    @Bean
+    fun objectMapper() = JsonMapper.builder()
+        .addModules(KotlinModule.Builder().build(), JavaTimeModule())
+        .enable(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN)
+        .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
+        .disable(
+            SerializationFeature.WRITE_DATES_AS_TIMESTAMPS,
+            SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS
+        )
+        .disable(
+            DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
+            DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS,
+            DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE
+        )
+        .build()
 }
