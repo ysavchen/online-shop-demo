398c9b3307eebd9eb98e252aa3a7452ab1495535
Yuriy Savchenko <ysavchen@outlook.com>
2024-09-21 03:45:56 +0300

    Add notes for bugs

--- 04-services/api-gateway/src/main/java/com/example/apigateway/AppRuntimeHints.java
+++ 04-services/api-gateway/src/main/java/com/example/apigateway/AppRuntimeHints.java
@@ -7,6 +7,7 @@ public class AppRuntimeHints implements RuntimeHintsRegistrar {
 
     @Override
     public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+        // must be registered by Spring Cloud Gateway, bug fix
         hints.resources().registerPattern("META-INF/scripts/request_rate_limiter.lua");
     }
 }
