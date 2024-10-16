7f5d5cd357eb7f69c244dab334c0322becd30fd6
Yuriy Savchenko <ysavchen@outlook.com>
2024-10-17 00:43:01 +0300

    Update Isbn model

--- 05-libraries/online-shop-model/src/main/kotlin/com/example/online/shop/model/Isbn.kt
+++ 05-libraries/online-shop-model/src/main/kotlin/com/example/online/shop/model/Isbn.kt
@@ -5,26 +5,33 @@ import com.example.online.shop.model.validation.requireFormat
 import com.example.online.shop.model.validation.requireRange
 
 @JvmInline
-value class Isbn(private val rawValue: String) {
+value class Isbn(private val rawValue: String) : Model {
+
+    init {
+        validate(rawValue)
+    }
 
     companion object {
         private const val MIN_LENGTH = 13
-        private const val MAX_LENGTH = 17
+        private const val MAX_LENGTH = 25
         private val isbnRegex = Regex(
             """
-                ^(?:ISBN(?:-1[03])?:? )?
-                (?=[0-9X]{10}${'$'}|
-                (?=(?:[0-9]+[- ]){3})[- 0-9X]{13}${'$'}|
-                97[89][0-9]{10}${'$'}|
-                (?=(?:[0-9]+[- ]){4})[- 0-9]{17}${'$'})
-                (?:97[89][- ]?)?[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9X]${'$'}
+               ^(?:ISBN(?:-13)?:? )?(?=[0-9]{13}${'$'}|(?=(?:[0-9]+[- ]){4})[- 0-9]{17}${'$'})97[89][- ]?[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9]${'$'}
             """.trimIndent()
         )
 
         fun validate(value: String): String = value
             .requireRange(MIN_LENGTH, MAX_LENGTH) {
-                throw IsbnValidationException("Invalid ISBN: $value. Length must be within $MIN_LENGTH and $MAX_LENGTH")
+                throw IsbnValidationException("Invalid ISBN: $value. Length is ${value.length}, but must be within $MIN_LENGTH and $MAX_LENGTH")
             }
             .requireFormat(isbnRegex) { throw IsbnValidationException("Invalid ISBN: $value") }
+
+        private fun format(value: String): String = TODO()
     }
+
+    /**
+     * ISBN 978-1-42051-505-3
+     */
+    override val formattedValue: String
+        get() = format(rawValue)
 }
---
+++ 05-libraries/online-shop-model/src/main/kotlin/com/example/online/shop/model/Model.kt
@@ -0,0 +1,8 @@
+package com.example.online.shop.model
+
+import java.io.Serializable
+
+interface Model : Serializable {
+
+    val formattedValue: String
+}
