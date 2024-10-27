0700b12bfd0b4a1cbf69bcebea531dc7d44cb998
Yuriy Savchenko <ysavchen@outlook.com>
2024-10-27 16:35:03 +0300

    Fix compile error

--- 05-libraries/adapters/build.gradle.kts
+++ 05-libraries/adapters/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    id("org.springframework.boot") version "3.3.4"
     id("io.spring.dependency-management") version "1.1.6"
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
@@ -31,20 +30,23 @@ allprojects {
 }
 
 subprojects {
+    apply {
+        plugin("org.jetbrains.kotlin.plugin.spring")
+        plugin("io.spring.dependency-management")
+    }
+
     dependencies {
         testImplementation(kotlin("test-junit5"))
         testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     }
 
+    dependencyManagement {
+        imports {
+            mavenBom("org.springframework.boot:spring-boot-dependencies:3.3.4")
+        }
+    }
+
     tasks.test {
         useJUnitPlatform()
-    }
-}
-
-configure(subprojects.filter { it.name.contains("starter") }) {
-    apply {
-        plugin("org.jetbrains.kotlin.plugin.spring")
-        plugin("org.springframework.boot")
-        plugin("io.spring.dependency-management")
     }
 }
