160ac9861ebb79482868a9171b9338f152c46f5f
Yuriy Savchenko <ysavchen@outlook.com>
2024-10-28 01:16:00 +0300

    Fix client properties

--- 05-libraries/adapters/book-service/book-service-rest-client-starter/src/main/kotlin/com/example/bookservice/rest/client/config/BookServiceClientProperties.kt
+++ 05-libraries/adapters/book-service/book-service-rest-client-starter/src/main/kotlin/com/example/bookservice/rest/client/config/BookServiceClientProperties.kt
@@ -1,10 +1,13 @@
 package com.example.bookservice.rest.client.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.NestedConfigurationProperty
 import java.time.Duration
 
 @ConfigurationProperties("application.client.book-service", ignoreUnknownFields = false)
 data class BookServiceClientProperties(
+
+    @NestedConfigurationProperty
     val http: HttpClientProperties
 )
 
