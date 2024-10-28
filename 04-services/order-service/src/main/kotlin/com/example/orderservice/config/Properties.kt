e12d4941166d4632210802c785c85e352deca8b9
Yuriy Savchenko <ysavchen@outlook.com>
2024-10-29 00:22:15 +0300

    Remove ignoreUnknownFields

--- 04-services/order-service/src/main/kotlin/com/example/orderservice/config/Properties.kt
+++ 04-services/order-service/src/main/kotlin/com/example/orderservice/config/Properties.kt
@@ -3,7 +3,7 @@ package com.example.orderservice.config
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.bind.DefaultValue
 
-@ConfigurationProperties("application", ignoreUnknownFields = false)
+@ConfigurationProperties("application")
 data class ApplicationProperties(
     @DefaultValue("UTC")
     val timezone: String
