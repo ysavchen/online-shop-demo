b2f32fcd3d550a8c3327df7e3e2dd740d7cb5634
Yuriy Savchenko <ysavchen@outlook.com>
2024-10-31 01:40:38 +0300

    Rough order-service client

---
+++ 05-libraries/adapters/order-service/build.gradle.kts
@@ -0,0 +1,16 @@
+project("order-service-kafka-client") {
+    dependencies {
+        api("org.jetbrains.kotlin:kotlin-reflect")
+        api("com.fasterxml.jackson.module:jackson-module-kotlin")
+        api("org.springframework.kafka:spring-kafka")
+        testImplementation("org.springframework.kafka:spring-kafka-test")
+    }
+}
+
+project("order-service-kafka-client-starter") {
+    dependencies {
+        api(project(":order-service:order-service-kafka-client"))
+        api("org.springframework.boot:spring-boot-starter")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+    }
+}
--- 05-libraries/adapters/settings.gradle.kts
+++ 05-libraries/adapters/settings.gradle.kts
@@ -2,5 +2,8 @@ rootProject.name = "adapters"
 
 include(
     "book-service:book-service-rest-client",
-    "book-service:book-service-rest-client-starter"
+    "book-service:book-service-rest-client-starter",
+
+    "order-service:order-service-kafka-client",
+    "order-service:order-service-kafka-client-starter"
 )
