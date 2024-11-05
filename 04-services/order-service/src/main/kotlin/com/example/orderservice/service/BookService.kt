d466ad4d29bcd18fe109cee7fc2fb3ccc89c30a0
Yuriy Savchenko <ysavchen@outlook.com>
2024-11-05 03:42:59 +0300

    Fix coroutines

--- 04-services/order-service/src/main/kotlin/com/example/orderservice/service/BookService.kt
+++ 04-services/order-service/src/main/kotlin/com/example/orderservice/service/BookService.kt
@@ -4,6 +4,7 @@ import com.example.bookservice.rest.client.BookServiceRestClient
 import com.example.orderservice.api.rest.OrderItemValidationException
 import com.example.orderservice.api.rest.model.OrderItem
 import com.example.orderservice.config.ApplicationProperties
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.springframework.stereotype.Service
 
@@ -17,14 +18,16 @@ class BookService(
         if (appProperties.features.bookValidation.enabled.not()) {
             return
         }
-        items.forEach { item ->
-            runBlocking {
-                val book = bookServiceClient.getBookById(item.id)
-                if (item.quantity > book.quantity) {
-                    throw OrderItemValidationException("Insufficient quantity of books (bookId=${item.id}): ordered=${item.quantity}, available in store=${book.quantity}")
-                }
-                if (item.price.value != book.price?.value) {
-                    throw OrderItemValidationException("Inconsistent book price (bookId=${item.id}): in order=${item.price.value}, in store=${book.price?.value}")
+        runBlocking {
+            items.forEach { item ->
+                launch {
+                    val book = bookServiceClient.getBookById(item.id)
+                    if (item.quantity > book.quantity) {
+                        throw OrderItemValidationException("Insufficient quantity of books (bookId=${item.id}): ordered=${item.quantity}, available in store=${book.quantity}")
+                    }
+                    if (item.price.value != book.price?.value) {
+                        throw OrderItemValidationException("Inconsistent book price (bookId=${item.id}): in order=${item.price.value}, in store=${book.price?.value}")
+                    }
                 }
             }
         }
