5df69c33e357fd407291a2da7191ddd312868b73
Yuriy Savchenko <ysavchen@outlook.com>
2024-11-06 00:19:35 +0300

    Update book-service-rest-client

--- 05-libraries/adapters/book-service/book-service-rest-client-starter/src/main/kotlin/com/example/bookservice/rest/client/config/BookServiceRestClientAutoConfiguration.kt
+++ 05-libraries/adapters/book-service/book-service-rest-client-starter/src/main/kotlin/com/example/bookservice/rest/client/config/BookServiceRestClientAutoConfiguration.kt
@@ -6,17 +6,19 @@ import io.netty.channel.ChannelOption
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.netty.http.client.HttpClient
 
+@Configuration(proxyBeanMethods = false)
 @EnableConfigurationProperties(BookServiceRestClientProperties::class)
 class BookServiceRestClientAutoConfiguration(private val properties: BookServiceRestClientProperties) {
 
     @Bean
-    @ConditionalOnMissingBean
+    @ConditionalOnMissingBean(BookServiceRestClient::class)
     fun bookServiceRestClient(bookServiceHttpClient: HttpClient): BookServiceRestClient =
         BookServiceRestClientImpl(
             WebClient.builder()
--- 05-libraries/adapters/book-service/book-service-rest-client-starter/src/main/kotlin/com/example/bookservice/rest/client/config/BookServiceRestClientProperties.kt
+++ 05-libraries/adapters/book-service/book-service-rest-client-starter/src/main/kotlin/com/example/bookservice/rest/client/config/BookServiceRestClientProperties.kt
@@ -2,6 +2,7 @@ package com.example.bookservice.rest.client.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.NestedConfigurationProperty
+import org.springframework.boot.context.properties.bind.DefaultValue
 import java.time.Duration
 
 @ConfigurationProperties("application.clients.book-service", ignoreUnknownFields = false)
@@ -12,7 +13,10 @@ data class BookServiceRestClientProperties(
 )
 
 data class HttpClientProperties(
+    @DefaultValue("http://localhost:8090")
     val baseUrl: String,
-    val connectionTimeout: Duration = Duration.ofSeconds(3),
-    val responseTimeout: Duration = Duration.ofSeconds(3)
+    @DefaultValue("3000")
+    val connectionTimeout: Duration,
+    @DefaultValue("3000")
+    val responseTimeout: Duration
 )
