2909450c535ad6a42b4e7924c7aa5f2919381f57
Yuriy Savchenko <ysavchen@outlook.com>
2024-11-30 10:55:12 +0300

    Process RequestDeliveryMessage

--- 04-services/delivery-service/src/main/kotlin/com/example/deliveryservice/mapping/DeliveryMapper.kt
+++ 04-services/delivery-service/src/main/kotlin/com/example/deliveryservice/mapping/DeliveryMapper.kt
@@ -1,4 +1,50 @@
 package com.example.deliveryservice.mapping
 
+import com.example.deliveryservice.api.kafka.client.model.CreateDeliveryRequestData
+import com.example.deliveryservice.api.kafka.client.model.Delivery
+import com.example.deliveryservice.api.kafka.client.model.Status
+import com.example.deliveryservice.api.kafka.client.model.Type
+import com.example.deliveryservice.repository.entity.DeliveryEntity
+import com.example.deliveryservice.repository.entity.StatusEntity
+import com.example.deliveryservice.repository.entity.TypeEntity
+import java.time.OffsetDateTime
+
 object DeliveryMapper {
+
+    internal fun CreateDeliveryRequestData.toEntity() = DeliveryEntity(
+        type = type.toEntity(),
+        status = StatusEntity.CREATED,
+        address = address,
+        orderId = orderId,
+        createdAt = OffsetDateTime.now(),
+        updatedAt = OffsetDateTime.now()
+    )
+
+    internal fun DeliveryEntity.toModel() = Delivery(
+        id = id!!,
+        type = type.toModel(),
+        status = status.toModel(),
+        address = address,
+        orderId = orderId
+    )
+
+    private fun Type.toEntity() = when (this) {
+        Type.HOME_DELIVERY -> TypeEntity.HOME_DELIVERY
+        Type.PICKUP_POINT -> TypeEntity.PICKUP_POINT
+        Type.PARCEL_LOCKER -> TypeEntity.PARCEL_LOCKER
+        Type.IN_STORE_PICKUP -> TypeEntity.IN_STORE_PICKUP
+    }
+
+    private fun TypeEntity.toModel() = when (this) {
+        TypeEntity.HOME_DELIVERY -> Type.HOME_DELIVERY
+        TypeEntity.PICKUP_POINT -> Type.PICKUP_POINT
+        TypeEntity.PARCEL_LOCKER -> Type.PARCEL_LOCKER
+        TypeEntity.IN_STORE_PICKUP -> Type.IN_STORE_PICKUP
+    }
+
+    private fun StatusEntity.toModel() = when (this) {
+        StatusEntity.CREATED -> Status.CREATED
+        StatusEntity.IN_PROGRESS -> Status.IN_PROGRESS
+        StatusEntity.DELIVERED -> Status.DELIVERED
+    }
 }
--- 04-services/delivery-service/src/main/kotlin/com/example/deliveryservice/service/DeliveryService.kt
+++ 04-services/delivery-service/src/main/kotlin/com/example/deliveryservice/service/DeliveryService.kt
@@ -1,21 +1,57 @@
 package com.example.deliveryservice.service
 
-import com.example.deliveryservice.api.kafka.client.model.RequestMessage
+import com.example.deliveryservice.api.kafka.client.ResponseDeliveryKafkaProducer
+import com.example.deliveryservice.api.kafka.client.model.CreateDeliveryRequest
+import com.example.deliveryservice.api.kafka.client.model.DeliveryCreatedResponse
+import com.example.deliveryservice.api.kafka.client.model.RequestDeliveryMessage
+import com.example.deliveryservice.mapping.DeliveryMapper.toEntity
+import com.example.deliveryservice.mapping.DeliveryMapper.toModel
 import com.example.deliveryservice.repository.DeliveryRepository
 import com.example.deliveryservice.repository.IdempotencyKeyRepository
+import com.example.deliveryservice.repository.entity.IdempotencyKeyEntity
+import com.example.deliveryservice.repository.entity.ResourceEntity
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
+import org.springframework.transaction.PlatformTransactionManager
+import org.springframework.transaction.support.TransactionTemplate
 import java.util.*
 
 @Service
 class DeliveryService(
     private val deliveryRepository: DeliveryRepository,
-    private val idempotencyKeyRepository: IdempotencyKeyRepository
+    private val idempotencyKeyRepository: IdempotencyKeyRepository,
+    private val transactionManager: PlatformTransactionManager,
+    private val kafkaProducer: ResponseDeliveryKafkaProducer
 ) {
 
-    @Transactional
-    fun createDelivery(message: ConsumerRecord<UUID, RequestMessage>) {
+    companion object {
+        private val logger = KotlinLogging.logger(DeliveryService::class.java.name)
+    }
 
+    private val transactionTemplate = TransactionTemplate(transactionManager)
+
+    fun processMessage(message: ConsumerRecord<UUID, RequestDeliveryMessage>) {
+        val key = idempotencyKeyRepository.findByIdOrNull(message.key())
+        if (key != null) {
+            logger.debug { "Duplicate message with key=${key.idempotencyKey}, message already processed" }
+            return
+        }
+
+        when (val request = message.value()) {
+            is CreateDeliveryRequest -> processRequest(message.key(), request)
+        }
+    }
+
+    private fun processRequest(messageKey: UUID, request: CreateDeliveryRequest) {
+        val delivery = transactionTemplate.execute {
+            val savedDelivery = deliveryRepository.save(request.data.toEntity())
+            idempotencyKeyRepository.save(
+                IdempotencyKeyEntity(messageKey, savedDelivery.id!!, ResourceEntity.DELIVERY)
+            )
+            savedDelivery.toModel()
+        }!!
+        kafkaProducer.send(DeliveryCreatedResponse(delivery))
     }
 }
