34d0f6244efaae4913f79207966471b1ca743978
Yuriy Savchenko <ysavchen@outlook.com>
2024-12-02 01:14:53 +0300

    Move client to starter

--- 05-libraries/adapters/order-service/build.gradle.kts
+++ 05-libraries/adapters/order-service/build.gradle.kts
@@ -1,16 +1,9 @@
-project("order-service-domain-kafka-client") {
+project("order-service-domain-kafka-client-starter") {
     dependencies {
         api("org.jetbrains.kotlin:kotlin-reflect")
         api("com.fasterxml.jackson.module:jackson-module-kotlin")
         api("org.springframework.kafka:spring-kafka")
-        testImplementation("org.springframework.kafka:spring-kafka-test")
-    }
-}
-
-project("order-service-domain-kafka-client-starter") {
-    dependencies {
-        api(project(":order-service:order-service-domain-kafka-client"))
-        api("org.springframework.kafka:spring-kafka")
+        api("org.springframework.kafka:spring-kafka-test")
         api("org.springframework.boot:spring-boot-starter")
         testImplementation("org.springframework.boot:spring-boot-starter-test")
     }
--- 05-libraries/adapters/settings.gradle.kts
+++ 05-libraries/adapters/settings.gradle.kts
@@ -4,7 +4,6 @@ include(
     "book-service:book-service-rest-client",
     "book-service:book-service-rest-client-starter",
 
-    "order-service:order-service-domain-kafka-client",
     "order-service:order-service-domain-kafka-client-starter",
 
     "delivery-service:delivery-service-model",
