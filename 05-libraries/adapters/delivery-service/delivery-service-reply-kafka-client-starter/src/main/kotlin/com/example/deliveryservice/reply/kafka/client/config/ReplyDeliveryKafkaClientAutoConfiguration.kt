33d791961e3a2eb79c0841d475eae39d52d813b6
Yuriy Savchenko <ysavchen@outlook.com>
2024-12-10 10:04:27 +0300

    Update auto-configurations

--- 05-libraries/adapters/delivery-service/delivery-service-reply-kafka-client-starter/src/main/kotlin/com/example/deliveryservice/reply/kafka/client/config/ReplyDeliveryKafkaClientAutoConfiguration.kt
+++ 05-libraries/adapters/delivery-service/delivery-service-reply-kafka-client-starter/src/main/kotlin/com/example/deliveryservice/reply/kafka/client/config/ReplyDeliveryKafkaClientAutoConfiguration.kt
@@ -50,7 +50,7 @@ class RequestDeliveryKafkaConsumerConfiguration(private val properties: ReplyDel
             mapOf(
                 ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to properties.kafka.connection.bootstrapServers.toList(),
                 ConsumerConfig.GROUP_ID_CONFIG to properties.kafka.replying.consumer.request.groupId,
-                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to OffsetResetStrategy.EARLIEST.name.lowercase(),
+                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to OffsetResetStrategy.EARLIEST.toString(),
                 ConsumerConfig.ALLOW_AUTO_CREATE_TOPICS_CONFIG to false
             ),
             ErrorHandlingDeserializer(UUIDDeserializer()).apply { isForKey = true },
--- 05-libraries/adapters/order-service/order-service-domain-kafka-client-starter/src/main/kotlin/com/example/orderservice/domain/kafka/client/config/DomainOrderKafkaClientAutoConfiguration.kt
+++ 05-libraries/adapters/order-service/order-service-domain-kafka-client-starter/src/main/kotlin/com/example/orderservice/domain/kafka/client/config/DomainOrderKafkaClientAutoConfiguration.kt
@@ -82,7 +82,7 @@ class DomainOrderKafkaConsumerConfiguration(private val properties: DomainOrderK
             mapOf(
                 ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to properties.kafka.connection.bootstrapServers.toList(),
                 ConsumerConfig.GROUP_ID_CONFIG to properties.kafka.domain.consumer!!.groupId,
-                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to OffsetResetStrategy.EARLIEST.name.lowercase(),
+                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to OffsetResetStrategy.EARLIEST.toString(),
                 ConsumerConfig.ALLOW_AUTO_CREATE_TOPICS_CONFIG to false
             ),
             ErrorHandlingDeserializer(UUIDDeserializer()).apply { isForKey = true },
