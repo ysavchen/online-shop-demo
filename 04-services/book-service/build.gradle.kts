a568228948941d0f05409798d052ad9e943c3e49
Yuriy Savchenko <ysavchen@outlook.com>
2024-12-30 23:56:11 +0300

    Upgrade spring-boot version

--- 04-services/book-service/build.gradle.kts
+++ 04-services/book-service/build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
-    id("org.springframework.boot") version "3.3.4"
-    id("io.spring.dependency-management") version "1.1.6"
-    id("org.graalvm.buildtools.native") version "0.10.3" apply false
+    java  //fix for plugin org.hibernate.orm
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("org.graalvm.buildtools.native") version "0.10.4" apply false
+    id("org.hibernate.orm") version "6.6.4.Final"
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.jpa") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
@@ -38,13 +40,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("org.postgresql:postgresql")
     implementation("org.liquibase:liquibase-core")
-    implementation("io.hypersistence:hypersistence-utils-hibernate-63:3.8.3")
-    kapt("org.hibernate.orm:hibernate-jpamodelgen:6.6.1.Final")
+    implementation("io.hypersistence:hypersistence-utils-hibernate-63:3.9.0")
+    kapt("org.hibernate.orm:hibernate-jpamodelgen:6.6.4.Final")
 
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
-    testImplementation("org.testcontainers:postgresql:1.20.2")
+    testImplementation("org.testcontainers:postgresql")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
@@ -56,7 +58,7 @@ dependencies {
     runtimeOnly("io.micrometer:micrometer-registry-prometheus")
     // Kotlin wrapper for slf4j-api
     // SLF4J and Logback comes pre-configured with Spring Boot
-    implementation("io.github.oshai:kotlin-logging-jvm:7.0.0")
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
     implementation("com.github.loki4j:loki-logback-appender:1.5.2")
     implementation("io.micrometer:micrometer-tracing-bridge-brave")
     // Dependency is needed for latency visualization
@@ -73,6 +75,18 @@ kotlin {
     compilerOptions {
         freeCompilerArgs.addAll("-Xjsr305=strict")
     }
+}
+
+hibernate {
+    enhancement {
+        enableAssociationManagement = true
+    }
+}
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
 }
 
 tasks.test {
