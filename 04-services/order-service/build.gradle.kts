02120f2bad6664c3db677b333a1330a56990ea59
Yuriy Savchenko <ysavchen@outlook.com>
2025-06-27 12:52:18 +0300

    Add production profile

--- 04-services/order-service/build.gradle.kts
+++ 04-services/order-service/build.gradle.kts
@@ -100,4 +100,5 @@ tasks.test {
 
 tasks.bootBuildImage {
     imageName = "$dockerHubRepository/${rootProject.name}:$version"
+    environment = mapOf("BPE_SPRING_PROFILES_ACTIVE" to "production")
 }
