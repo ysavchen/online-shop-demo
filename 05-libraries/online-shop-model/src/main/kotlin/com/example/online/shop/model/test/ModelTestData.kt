d4ec7c4909b79080f5bfada9b8e193fa61e4d9f3
Yuriy Savchenko <ysavchen@outlook.com>
2025-11-30 17:50:57 +0300

    Update ModelTestData

--- 05-libraries/online-shop-model/src/main/kotlin/com/example/online/shop/model/test/ModelTestData.kt
+++ 05-libraries/online-shop-model/src/main/kotlin/com/example/online/shop/model/test/ModelTestData.kt
@@ -1,16 +1,21 @@
 package com.example.online.shop.model.test
 
 import com.example.online.shop.model.*
+import com.example.online.shop.model.PriceValueUtils.MAX_PRICE_VALUE
+import com.example.online.shop.model.PriceValueUtils.MIN_PRICE_VALUE
 import org.apache.commons.lang3.RandomStringUtils
+import java.math.BigDecimal
+import kotlin.random.Random
 
 object ModelTestData {
 
-    private val authorRange = AuthorUtils.MIN_LENGTH..AuthorUtils.MAX_LENGTH
-    private val buildingRange = BuildingUtils.MIN_LENGTH..BuildingUtils.MAX_LENGTH
-    private val cityRange = CityUtils.MIN_LENGTH..CityUtils.MAX_LENGTH
-    private val countryRange = CountryUtils.MIN_LENGTH..CountryUtils.MAX_LENGTH
-    private val descriptionRange = DescriptionUtils.MIN_LENGTH..DescriptionUtils.MAX_LENGTH
-    private val priceValueRange = PriceValueUtils.MIN_PRICE_VALUE..PriceValueUtils.MAX_PRICE_VALUE
+    private val randomAuthorLength = (AuthorUtils.MIN_LENGTH..AuthorUtils.MAX_LENGTH).random()
+    private val randomBuildingLength = (BuildingUtils.MIN_LENGTH..BuildingUtils.MAX_LENGTH).random()
+    private val randomCityLength = (CityUtils.MIN_LENGTH..CityUtils.MAX_LENGTH).random()
+    private val randomCountryLength = (CountryUtils.MIN_LENGTH..CountryUtils.MAX_LENGTH).random()
+    private val randomDescriptionLength = (DescriptionUtils.MIN_LENGTH..DescriptionUtils.MAX_LENGTH).random()
+    private val randomPriceValue =
+        Random.nextDouble(MIN_PRICE_VALUE.toDouble(), MAX_PRICE_VALUE.toDouble()).toBigDecimal()
     private val quantityRange = QuantityUtils.MIN_QUANTITY..QuantityUtils.MAX_QUANTITY
     private val ratingRange = RatingUtils.MIN_RATING..RatingUtils.MAX_RATING
     private val reviewTextRange = ReviewTextUtils.MIN_LENGTH..ReviewTextUtils.MAX_LENGTH
@@ -20,8 +25,13 @@ object ModelTestData {
 
     private val randomString = RandomStringUtils.insecure()
 
-    fun author(length: Int = authorRange.random()): Author = Author(randomString.nextAlphabetic(length))
-    fun building(length: Int = buildingRange.random()): Building = Building(randomString.nextAlphabetic(length))
-    fun city(length: Int = cityRange.random()): City = City(randomString.nextAlphabetic(length))
+    fun author(length: Int = randomAuthorLength): Author = Author(randomString.nextAlphabetic(length))
+    fun building(length: Int = randomBuildingLength): Building = Building(randomString.nextAlphabetic(length))
+    fun city(length: Int = randomCityLength): City = City(randomString.nextAlphabetic(length))
+    fun country(length: Int = randomCountryLength): Country = Country(randomString.nextAlphabetic(length))
+    fun description(length: Int = randomDescriptionLength): Description =
+        Description(randomString.nextAlphabetic(length))
+
+    fun priceValue(value: BigDecimal = randomPriceValue) = PriceValue(value)
 
 }
