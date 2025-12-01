3352d2647e326acc1835cd4a960593b4eedd158f
Yuriy Savchenko <ysavchen@outlook.com>
2025-12-01 04:46:55 +0300

    Fix test

--- 05-libraries/online-shop-model/src/test/kotlin/com/example/online/shop/model/IsbnTests.kt
+++ 05-libraries/online-shop-model/src/test/kotlin/com/example/online/shop/model/IsbnTests.kt
@@ -68,7 +68,7 @@ class IsbnTests {
         val expectedValue = "978-1-42051-505-3"
 
         numbers.forEach {
-            assertEquals(expectedValue, Isbn.valueOf(it).formattedValue)
+            assertEquals(expectedValue, Isbn.valueOf(it).value)
         }
     }
 }
