a336e10b5761ed4aea49a5b01e751055f813469e
Yuriy Savchenko <ysavchen@outlook.com>
2025-12-01 05:49:38 +0300

    Update tests

--- 04-services/book-service/src/test/kotlin/com/example/bookservice/test/TestData.kt
+++ 04-services/book-service/src/test/kotlin/com/example/bookservice/test/TestData.kt
@@ -13,6 +13,7 @@ import com.example.online.shop.model.test.ModelTestData.rating
 import com.example.online.shop.model.test.ModelTestData.reviewText
 import com.example.online.shop.model.test.ModelTestData.title
 import com.example.orderservice.domain.kafka.client.model.*
+import java.math.BigDecimal
 import java.time.OffsetDateTime
 import java.util.*
 
@@ -23,50 +24,50 @@ object BookTestData {
 
     fun createBookRequest() = CreateBookRequest(
         isbn = Isbn("9781525826689"),
-        title = title(),
-        authors = listOf(author()),
-        description = description(),
+        title = title(15),
+        authors = listOf(author(10)),
+        description = description(25),
         genre = nextValue<Genre>(),
         releaseDate = randomLocalDate(),
-        quantity = quantity(),
-        price = Price(priceValue(), nextValue<RestCurrency>())
+        quantity = quantity(3),
+        price = Price(priceValue(BigDecimal.valueOf(250)), nextValue<RestCurrency>())
     )
 
     fun updateBookRequest() = UpdateBookRequest(
         releaseDate = randomLocalDate(),
-        quantity = quantity(),
-        price = Price(priceValue(), nextValue<RestCurrency>())
+        quantity = quantity(3),
+        price = Price(priceValue(BigDecimal.valueOf(250)), nextValue<RestCurrency>())
     )
 
     fun bookEntity(
         isbn: Isbn = Isbn("9781525826689")
     ) = BookEntity(
         isbn = isbn,
-        title = title(),
-        authors = arrayOf(author().value),
-        description = description(),
+        title = title(15),
+        authors = arrayOf(author(10).value),
+        description = description(25),
         genre = nextValue<GenreEntity>(),
         releaseDate = randomLocalDate(),
-        quantity = quantity(),
-        price = PriceEntity(priceValue().value, nextValue<CurrencyEntity>())
+        quantity = quantity(3),
+        price = PriceEntity(priceValue(BigDecimal.valueOf(250)).value, nextValue<CurrencyEntity>())
     )
 }
 
 object ReviewTestData {
 
     fun createReviewRequest(bookId: UUID) = CreateReviewRequest(
-        title = title(),
-        reviewText = reviewText(),
-        author = author(),
-        rating = rating(),
+        title = title(15),
+        reviewText = reviewText(25),
+        author = author(10),
+        rating = rating(BigDecimal.valueOf(5)),
         bookId = bookId
     )
 
     fun reviewEntity(bookFk: UUID) = ReviewEntity(
-        title = title(),
-        reviewText = reviewText(),
-        author = author(),
-        rating = rating(),
+        title = title(15),
+        reviewText = reviewText(25),
+        author = author(10),
+        rating = rating(BigDecimal.valueOf(5)),
         bookFk = bookFk
     )
 }
@@ -96,7 +97,7 @@ object OrderTestData {
     ) = OrderItem(
         id = book.id,
         category = ItemCategory.BOOKS,
-        quantity = quantity(),
+        quantity = quantity(3),
         price = ItemPrice(
             value = book.price!!.value,
             currency = ItemCurrency.RUB
