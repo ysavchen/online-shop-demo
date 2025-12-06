2d247eaa36e4540ca81ae86c1afdf49bde68d3d4
Yuriy Savchenko <ysavchen@outlook.com>
2025-12-06 13:53:36 +0300

    Upgrade Spring Boot

--- 04-services/delivery-service/build.gradle.kts
+++ 04-services/delivery-service/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     java  //fix for plugin org.hibernate.orm
-    id("org.springframework.boot") version "3.4.6"
+    id("org.springframework.boot") version "4.0.0"
     id("io.spring.dependency-management") version "1.1.7"
-    id("org.graalvm.buildtools.native") version "0.10.6" apply false
-    id("org.hibernate.orm") version "6.6.15.Final"
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.jpa") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
+    id("org.graalvm.buildtools.native") version "0.11.3" apply false
+    id("org.hibernate.orm") version "7.1.8.Final"
+    kotlin("jvm") version "2.2.21"
+    kotlin("plugin.jpa") version "2.2.21"
+    kotlin("plugin.spring") version "2.2.21"
 }
 
 group = "com.example"
@@ -29,29 +29,28 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
     // Web
-    implementation("org.springframework.boot:spring-boot-starter-web") {
-        exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat")
-    }
-    implementation("org.springframework.boot:spring-boot-starter-undertow")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.20.1")
 
     // Postgres
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("org.postgresql:postgresql")
     implementation("org.liquibase:liquibase-core")
-    implementation("io.hypersistence:hypersistence-utils-hibernate-63:3.9.11")
+    implementation("io.hypersistence:hypersistence-utils-hibernate-63:3.13.2")
 
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
-    testImplementation("org.testcontainers:postgresql")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("org.testcontainers:testcontainers-junit-jupiter")
+    testImplementation("org.testcontainers:testcontainers-postgresql")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     // Observability
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("io.github.oshai:kotlin-logging-jvm:7.0.7")
-    implementation("com.github.loki4j:loki-logback-appender:2.0.0")
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.13")
+    implementation("com.github.loki4j:loki-logback-appender:2.0.1")
     implementation("io.micrometer:micrometer-tracing-bridge-brave")
     implementation("io.zipkin.reporter2:zipkin-reporter-brave")
     runtimeOnly("io.micrometer:micrometer-registry-prometheus")
@@ -62,7 +61,7 @@ dependencies {
 
 kotlin {
     compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict")
+        freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
     }
 }
 
