5ed10d4b540f0077873170dd802c054e6fae732a
Yuriy Savchenko <ysavchen@outlook.com>
2025-12-17 16:22:41 +0300

    Update jackson configuration

--- 05-libraries/service-support/src/main/kotlin/com/example/service/support/jackson/JacksonSupportConfiguration.kt
+++ 05-libraries/service-support/src/main/kotlin/com/example/service/support/jackson/JacksonSupportConfiguration.kt
@@ -1,28 +1,26 @@
-package com.example.service.support.jackson.autoconfigure
+package com.example.service.support.jackson
 
-import org.springframework.boot.autoconfigure.AutoConfiguration
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import tools.jackson.core.StreamWriteFeature
 import tools.jackson.databind.DeserializationFeature
 import tools.jackson.databind.MapperFeature
 import tools.jackson.databind.cfg.DateTimeFeature
 import tools.jackson.databind.json.JsonMapper
-import tools.jackson.databind.util.StdDateFormat.DATE_FORMAT_STR_ISO8601
+import tools.jackson.databind.util.StdDateFormat
 import tools.jackson.datatype.jsr310.JavaTimeModule
 import tools.jackson.module.kotlin.KotlinModule
 import java.text.SimpleDateFormat
 import java.util.*
 
-@AutoConfiguration
-class JacksonSupportAutoConfiguration {
+@Configuration
+class JacksonSupportConfiguration {
 
     @Bean
-    @ConditionalOnMissingBean
     fun jsonMapper(): JsonMapper = JsonMapper.builder()
         .addModules(KotlinModule.Builder().build(), JavaTimeModule())
         .defaultTimeZone(TimeZone.getDefault())
-        .defaultDateFormat(SimpleDateFormat(DATE_FORMAT_STR_ISO8601))
+        .defaultDateFormat(SimpleDateFormat(StdDateFormat.DATE_FORMAT_STR_ISO8601))
         .enable(StreamWriteFeature.WRITE_BIGDECIMAL_AS_PLAIN)
         .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
         .disable(
