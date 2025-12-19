347868c7fa62074e3b2744171a707b46cd0d8837
Yuriy Savchenko <ysavchen@outlook.com>
2025-12-19 12:13:41 +0300

    Use common error codes

--- 04-services/book-service/src/main/kotlin/com/example/bookservice/api/rest/Exceptions.kt
+++ 04-services/book-service/src/main/kotlin/com/example/bookservice/api/rest/Exceptions.kt
@@ -1,6 +1,7 @@
 package com.example.bookservice.api.rest
 
-import com.example.bookservice.api.rest.model.ErrorCode
+import com.example.service.support.error.CommonErrorCode
+import com.example.service.support.error.ErrorCode
 import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatusCode
 import java.util.*
@@ -12,17 +13,17 @@ open class ServiceException(
 ) : RuntimeException(message)
 
 class BookNotFoundException(id: UUID) :
-    ServiceException("Book not found by id=$id", ErrorCode.RESOURCE_NOT_FOUND, HttpStatus.NOT_FOUND)
+    ServiceException("Book not found by id=$id", CommonErrorCode.RESOURCE_NOT_FOUND, HttpStatus.NOT_FOUND)
 
 class ReviewNotFoundException(id: UUID) :
-    ServiceException("Review not found by id=$id", ErrorCode.RESOURCE_NOT_FOUND, HttpStatus.NOT_FOUND)
+    ServiceException("Review not found by id=$id", CommonErrorCode.RESOURCE_NOT_FOUND, HttpStatus.NOT_FOUND)
 
 class RequestValidationException(message: String) :
-    ServiceException(message, ErrorCode.REQUEST_VALIDATION_ERROR, HttpStatus.BAD_REQUEST)
+    ServiceException(message, CommonErrorCode.REQUEST_VALIDATION_ERROR, HttpStatus.BAD_REQUEST)
 
 class DuplicateRequestException(idempotencyKey: UUID, resourceId: UUID, resource: String) :
     ServiceException(
         "Duplicate request with idempotencyKey=$idempotencyKey, $resource already created with id=$resourceId",
-        ErrorCode.REQUEST_ALREADY_PROCESSED,
+        CommonErrorCode.REQUEST_ALREADY_PROCESSED,
         HttpStatus.CONFLICT
     )
